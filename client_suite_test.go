72043e8367da8507322f9ab44f1f3f5a32f5727b
Maxwell Da Silva <dayvson@gmail.com>
2016-11-08 22:15:12 -0500

    add StartFakeServer function to intercept http requests

--- client_suite_test.go
+++ client_suite_test.go
@@ -4,10 +4,20 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestUploaders(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Client Suite")
+}
+
+func StartFakeServer(status int, content string) *httptest.Server {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(status)
+		w.Write([]byte(content))
+	}))
+	return server
 }
